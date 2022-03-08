b6e885be4ba03aea63fdd43529b16e90a3306b46
Stetics Seinen <maxira626@gmail.com>
2022-03-09 01:05:27 +0300

    final

--- hello_world.cpp
+++ hello_world.cpp
@@ -8,15 +8,8 @@ main()
   std::string @name;
 
   std::cout << "Name: ";
-
-<<<<<<< HEAD
   std::cin >> @name; //ввод
   std::cout << endl; //переход
-=======
-std::cout << "Name: ";
-std::cin >> @name;//ввод
-std::cout<< endl;//переход
->>>>>>> 8e8d1057dbe1b9ebe73bd22b1994131cd465dfe8
 
   std::cout << "hello world from " << @name << std::endl; //вывод
 
