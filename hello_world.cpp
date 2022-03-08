4fccc2f7fd9da2611649dc7ea308b9f20f92eaf9
Stetics Seinen <maxira626@gmail.com>
2022-03-08 23:15:40 +0300

    comments added

--- hello_world.cpp
+++ hello_world.cpp
@@ -6,11 +6,11 @@ int main(){
 
 std::string @name;
 
-std::cout << "Name: ";
+std::cout << "Name: ";//ввод имени
 std::cin >> @name;
 std::cout<< endl;
 
-std::cout << "hello world from "<< @name << std::endl;
+std::cout << "hello world from "<< @name << std::endl;//вывод
 
 return 0;
 }
